a19e61b984583e0b0ee96ad3b249463c0ba3fa66
Swayam Khandelwal <114726617+Swayamkh02@users.noreply.github.com>
2024-12-30 21:15:16 +0530

    Fix/Fixes

--- frontend/src/pages/QPGenerate.js
+++ frontend/src/pages/QPGenerate.js
@@ -57,7 +57,7 @@ const QPGenerate = () => {
       formData.append('file', files[0]); // Attach the first file
       formData.append('data', JSON.stringify(payload)); // Attach JSON payload
 
-      const response = await fetch('https://qpgen-lgap.onrender.com/generate_qp', {
+      const response = await fetch('https://qpgen-l.onrender.com/generate_qp', {
         method: 'POST',
         body: formData, // Send formData
       });
